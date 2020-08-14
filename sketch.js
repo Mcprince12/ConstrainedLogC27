6ec2240a2ecd8ba69e96bb1552098af4a713ddb1
Mcprince12 <sohumtrip@gmail.com>
2020-08-14 12:02:24 +0200

    We made a log attached to the bird

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6, chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,8 +35,11 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(200, 200, 168, -PI/5);
 
     bird = new Bird(100,100);
+
+    chain = new Chain(bird.body, log6.body);
 
 }
 
@@ -61,4 +66,8 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    log6.display();
+        
+    chain.display();
 }
